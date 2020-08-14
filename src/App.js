69383a40614f0f79b9815a7a712506b89ceb334a
Taegyun Kim <tagun1202@gmail.com>
2020-08-14 12:27:06 +0900

    fix: hard coded posts are now changed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Post from './Post.js';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "tg",
+      caption: "Check1",
+      imageUrl: "https://images.unsplash.com/photo-1497515114629-f71d768fd07c?ixlib=rb-1.2.1&w=1000&q=80"
+    },
+    {
+      username: "kim",
+      caption: "check2",
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/3D_medical_animation_corona_virus.jpg/800px-3D_medical_animation_corona_virus.jpg"
+    },
+    {
+      username: "test",
+      caption: "Pig",
+      imageUrl: "https://images-na.ssl-images-amazon.com/images/I/61WdixjupoL._AC_SX679_.jpg"
+    },
+    {
+      username: "who am i",
+      caption: "I'm kim",
+      imageUrl: "https://www.ie.edu/exponential-learning/blog/wp-content/uploads/2018/01/MachineLearninginMarketing-1621x1000.jpg"
+    }
+  ]);
   return (
     <div className="app">
       <div className="app__header">
         <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="insta logo" />
       </div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        )
+        )
+      }
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Post.css';
 import Avatar from '@material-ui/core/Avatar';
 
-function Post() {
+function Post({username, caption, imageUrl}) {
   return (
     <div className="post">
       <div className="post__header">
@@ -11,11 +11,10 @@ function Post() {
           alt="hi"
           src="/static/images/avatar/1.jps"
         />
-        <h3>Username</h3>
+        <h3>{username}</h3>
       </div>
-      {/*header -> avatar & user_name */}
-      <img className="post__img" src="https://images.unsplash.com/photo-1497515114629-f71d768fd07c?ixlib=rb-1.2.1&w=1000&q=80" alt="coffee" />
-      <h4 className="post__text"> <strong>Username</strong> caption</h4>
+      <img className="post__img" src={imageUrl} alt="coffee" />
+      <h4 className="post__text"> <strong>{username}</strong> {caption}</h4>
     </div>
   )
 }
